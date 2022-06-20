af8539e77c5c3715bf2b90eaaa3bb2c8dfb745b9
Alejandro Cerezo <alce65@hotmail.es>
2022-06-20 13:55:34 +0200

    Initial server and fileRead

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hola Mundo');
+import fs from 'fs';
+import fsp from 'fs/promises';
+import http from 'http';
+import url from 'url';
+
+const PORT = process.env.PORT || 3204;
+
+// const buffer = fs.readFileSync('./data/data.txt', { encoding: 'utf-8' });
+// console.log(buffer);
+
+export const server = http.createServer((req, res) => {
+    const path = url.parse(req.url as string).path;
+    let dataFile: string = `./data/${path}.txt`;
+    fsp.readFile();
+    /* fs.readFile(dataFile, { encoding: 'utf-8' }, (err, data) => {
+        if (err) {
+            res.end('Error de lectura');
+            console.log(err.message);
+            return;
+        }
+        console.log(data);
+        const template = `<h1>Hola Mundo</h1><p>${data}</p>`;
+        res.end(template);
+    }); */
+});
+server.listen(PORT);
+console.log(`Server listening in port ${PORT}`);
